3123ec8474734177e022530de5c6475abc2baeaa
trackerMGN <meshworks7@gmail.com>
2022-11-22 13:24:52 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import {Footer, Header, Blog, Possibility, Features, WhatGPT3,} from './cointainers';
+import {CTA, Brand, Navbar} from './components';
+import './App.css';
+const App = () => {
+  return (
+    <div className='App'>
+        <div className='gradient__bg'>
+          <Navbar/>
+          <Header/>
+        </div>
+        <Brand/>
+        <WhatGPT3/>
+        <Features/>
+        <Possibility/>
+        <CTA/>
+        <Blog/>
+        <Footer/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/cointainers/whatGPT3/WhatGPT3.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import { Feature } from '../../components';
+import './whatGPT3.css';
+const WhatGPT3 = () => {
+  return (
+    <div className='gpt3__whatgpt3 section__margin' id='wgpt3'>
+      <div className='gpt3__whatgpt3-features'>
+        <Feature title='What is GPT-3' text='We so opinion friends me message as delight. Whole front do of plate heard oh ought. His defective nor convinced residence own. Connection has put impossible own apartments boisterous. At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by.'/>
+      </div>
+      <div className='gpt3__whatgpt3-heading'>
+        <h1 className='gradient__text'>The Possibilities are beyond your imagination</h1>
+        <p>Explore The Library</p>
+      </div>
+      <div className='gpt3__whatgpt3-container'>
+        <Feature title='Chatbots' text='We so opinion friends me message as delight. Whole front do of plate heard oh ought.'/>
+        <Feature title='Knowledgbase' text='At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by. As put impossible own apartments.'/>
+        <Feature title='Education' text='At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by. As put impossible own apartments.'/>
+      </div>
+    </div>
+  )
+}
+
+export default WhatGPT3
